c1d0085371d57f4164f608f1e77306f525f69b63
jinpython <1114617379@qq.com>
2018-06-22 17:18:29 +0800

    fix compiling errors

--- src/proc/proc_kv.cpp
+++ src/proc/proc_kv.cpp
@@ -10,15 +10,14 @@ int proc_get(DbEngine* db, const Request & request, Response & response) {
     std::string val;
     int result = db->get(request[0], val);
     response.push_back(val);
-    return result;
+    return result == DbEngine::DB_OK ? Proc::PROCESS_OK : Proc::PROCESS_ERROR_MSG;
 }
 
 int proc_set(DbEngine* db, const Request & request, Response & response) {
     std::string resp;
     int result = db->set(request[0], request[1], resp);
-    if (result == DbEngine::DB_OK) {
-        response.push_back(resp);
-    }
+    response.push_back(resp);
+    return result == DbEngine::DB_OK ? Proc::PROCESS_OK : Proc::PROCESS_ERROR_MSG;
 }
 
 int proc_getrange(DbEngine* db, const Request & request, Response & response) {
@@ -26,5 +25,6 @@ int proc_getrange(DbEngine* db, const Request & request, Response & response) {
     int start = std::stoi(request[1]);
     int end = std::stoi(request[2]);
     int result = db->getrange(request[0], val, start, end);
-
+    response.push_back(val);
+    return result == DbEngine::DB_OK ? Proc::PROCESS_OK : Proc::PROCESS_ERROR_MSG;
 }
